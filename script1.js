ff84c710abdb8fc8ab91c3c4bb2bb98d4c9e49e8
srishti-tech <64358454+srishti-tech@users.noreply.github.com>
2024-05-26 18:25:45 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,20 +1,19 @@
-
-const apiKey = '32a8b6a82885431242c1205cb7b288b0'
+const apiKey = "32a8b6a82885431242c1205cb7b288b0";
 async function fetchWeatherData(city) {
-    try {
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-        );
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    );
 
-        if (!response.ok) {
-            throw new Error("Unable to fetch weather data");
-        }
-        const data = await response.json();
-        console.log(data);
-        updateWeatherUI(data);
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      throw new Error("Unable to fetch weather data");
     }
+    const data = await response.json();
+    console.log(data);
+    updateWeatherUI(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const cityElement = document.querySelector(".city");
@@ -27,49 +26,47 @@ const descriptionText = document.querySelector(".description-text");
 const date = document.querySelector(".date");
 const descriptionIcon = document.querySelector(".description i");
 
-
 function updateWeatherUI(data) {
-    
-    cityElement.textContent = "Weather of "+ data.name;
-    temperature.textContent = `${Math.round(data.main.temp)}`+"°C";
-    windSpeed.textContent = `${data.wind.speed} m/s W`;
-    humidity.textContent = `${data.main.humidity}%`;
-    visibility.textContent = `${data.visibility / 1000} km`;
-    descriptionText.textContent = data.weather[0].description;
-    pressure.textContent = `${data.main.pressure} mb`;
+  cityElement.textContent = "Weather of " + data.name;
+  temperature.textContent = `${Math.round(data.main.temp)}` + "°C";
+  windSpeed.textContent = `${data.wind.speed} m/s W`;
+  humidity.textContent = `${data.main.humidity}%`;
+  visibility.textContent = `${data.visibility / 1000} km`;
+  descriptionText.textContent = data.weather[0].description;
+  pressure.textContent = `${data.main.pressure} mb`;
 
-    const currentDate = new Date();
-    date.textContent = currentDate.toDateString();
-    const weatherIconName = getWeatherIconName(data.weather[0].main);
-    descriptionIcon.innerHTML = `<i class="material-icons">${weatherIconName}</i>`;
+  const currentDate = new Date();
+  date.textContent = currentDate.toDateString();
+  const weatherIconName = getWeatherIconName(data.weather[0].main);
+  descriptionIcon.innerHTML = `<i class="material-icons">${weatherIconName}</i>`;
 }
 
 const formElement = document.querySelector(".search-form");
 const inputElement = document.querySelector(".city-input");
 
 formElement.addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const city = inputElement.value;
-    if (city !== "") {
-        fetchWeatherData(city);
-        inputElement.value = "";
-    }
+  const city = inputElement.value;
+  if (city !== "") {
+    fetchWeatherData(city);
+    inputElement.value = "";
+  }
 });
 
 function getWeatherIconName(weatherCondition) {
-    const iconMap = {
-        Clear: "wb_sunny",
-        Clouds: "wb_cloudy",
-        Rain: "umbrella",
-        Thunderstorm: "flash_on",
-        Drizzle: "grain",
-        Snow: "ac_unit",
-        Mist: "cloud",
-        Smoke: "cloud",
-        Haze: "cloud",
-        Fog: "cloud",
-    };
+  const iconMap = {
+    Clear: "wb_sunny",
+    Clouds: "wb_cloudy",
+    Rain: "umbrella",
+    Thunderstorm: "flash_on",
+    Drizzle: "grain",
+    Snow: "ac_unit",
+    Mist: "cloud",
+    Smoke: "cloud",
+    Haze: "cloud",
+    Fog: "cloud",
+  };
 
-    return iconMap[weatherCondition] || "help";
+  return iconMap[weatherCondition] || "help";
 }
